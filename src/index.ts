c8d8cda7a29e55db34273b6ed2aad3782ba3138c
Rishi Gupta <rishig@zulipchat.com>
2019-07-22 15:27:22 -0700

    Remove extra spaces around parens.

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,17 @@ const app = express();
 const port = 8080; // default port to listen
 
 // Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" );
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+app.get("/", (req, res) => {
     // render the index template
-    res.render( "index" );
+    res.render("index");
 } );
 
 // start the express server
-app.listen( port, () => {
+app.listen(port, () => {
     // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+    console.log(`server started at http://localhost:${ port }`);
+});
